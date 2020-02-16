89af8a728c5deceb75f77100fdadb9ff51d03b4c
Stefan <stevetv90@gmail.com>
2020-02-16 22:04:55 +0100

    Title update

--- conf.lua
+++ conf.lua
@@ -3,7 +3,7 @@ function love.conf(t)
     t.version = "0.9.2"                -- The LÖVE version this game was made for (string)
     t.console = false                  -- Attach a console (boolean, Windows only)
  
-    t.window.title = "Test"        -- The window title (string)
+    t.window.title = "3D Sokoban"      -- The window title (string)
     t.window.icon = nil                -- Filepath to an image to use as the window's icon (string)
     t.window.width = 1280              -- The window width (number)
     t.window.height = 720              -- The window height (number)
